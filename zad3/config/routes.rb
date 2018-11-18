741879dd3f7aba875e089e5e4c70632642570887
rupinski75 <33001700+rupinski75@users.noreply.github.com>
2018-11-18 10:19:03 +0100

    Revert "Add zad 3"
    This reverts commit 4deb77adc905de259f5518fcf42c9697cbdaa330.

--- zad3/config/routes.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  get 'bestiary_controller/index'
-  get 'bestiary_controller/novigrad_monsters'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
